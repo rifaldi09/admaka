d194f54ece9d3fd549625d56506ec26458196cd3
Kika Haekal <91299160+Kikahaekal@users.noreply.github.com>
2025-05-11 10:05:48 +0700

    fix seeder

--- database/seeders/RoleAksesSeeder.php
+++ database/seeders/RoleAksesSeeder.php
@@ -17,7 +17,6 @@ class RoleAksesSeeder extends Seeder
             ['id_role' => 1, 'id_menu' => 1, 'created_at' => now(), 'updated_at' => now()],
             ['id_role' => 1, 'id_menu' => 8, 'created_at' => now(), 'updated_at' => now()],
             ['id_role' => 1, 'id_menu' => 4, 'created_at' => now(), 'updated_at' => now()],
-            ['id_role' => 1, 'id_menu' => 9, 'created_at' => now(), 'updated_at' => now()],
             ['id_role' => 3, 'id_menu' => 1, 'created_at' => now(), 'updated_at' => now()],
             ['id_role' => 3, 'id_menu' => 2, 'created_at' => now(), 'updated_at' => now()],
             ['id_role' => 3, 'id_menu' => 3, 'created_at' => now(), 'updated_at' => now()],
@@ -25,9 +24,8 @@ class RoleAksesSeeder extends Seeder
             ['id_role' => 3, 'id_menu' => 5, 'created_at' => now(), 'updated_at' => now()],
             ['id_role' => 3, 'id_menu' => 6, 'created_at' => now(), 'updated_at' => now()],
             ['id_role' => 3, 'id_menu' => 7, 'created_at' => now(), 'updated_at' => now()],
-            ['id_role' => 3, 'id_menu' => 8, 'created_at' => now(), 'updated_at' => now()],
-            ['id_role' => 3, 'id_menu' => 10, 'created_at' => now(), 'updated_at' => now()],
-            ['id_role' => 5, 'id_menu' => 11, 'created_at' => now(), 'updated_at' => now()]
+            ['id_role' => 3, 'id_menu' => 9, 'created_at' => now(), 'updated_at' => now()],
+            ['id_role' => 5, 'id_menu' => 10, 'created_at' => now(), 'updated_at' => now()]
         ]);
     }
 }
