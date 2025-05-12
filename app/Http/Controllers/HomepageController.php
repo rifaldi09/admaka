726d4ad4f8ed6dc7b374faa548b9bd57a69c20e1
Rifaldi Herikson <53487938+rifaldi09@users.noreply.github.com>
2025-05-12 23:50:53 +0700

    Update HomepageController.php

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -6,7 +6,7 @@ class HomepageController extends Controller
 {
     public function index()
     {
-        return view('homepage.index');
+        return view('homepage.index', ['title' => 'ADMAKA - FTTK UMRAH']);
     }
 
     // untuk kehalaman dashboard
